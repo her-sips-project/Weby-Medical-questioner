5d6fdead8c60bb36b68a5753ee89bd558b1e18c0
Ruth_BenTov_developer <112100490+RuthBenTov@users.noreply.github.com>
2023-12-19 09:51:37 +0200

    mongo

--- backend/server.ts
+++ backend/server.ts
@@ -1,21 +1,25 @@
 import express from "express";
+const mongoose = require("mongoose");
+
 const dotenv = require("dotenv");
-const cors = require("cors")
+const { mongo_URI } = process.env;
+const cors = require("cors");
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-
-
 app.use(express.json());
 app.use(cors());
 
-import emailRoutes from "./API/emailRoutes"
+import emailRoutes from "./API/emailRoutes";
 app.use("/api/v1/email", emailRoutes);
 
+mongoose.connect(`${mongo_URI}`).then(() => {
+  console.log("Connected to MongoDB!!!");
+}).catch(err => console.error(err));
 
 const server = app.listen(PORT, () => {
-    console.log(`server is listening in port:${PORT} :)`)
-})
+  console.log(`server is listening in port:${PORT} :)`);
+});
