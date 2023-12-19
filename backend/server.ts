84486599e68614eea25aed675baadfd7c4cd5ad2
hadari1996 <hadarzocaim@gmail.com>
2023-12-19 22:13:46 +0200

    all changes

--- backend/server.ts
+++ backend/server.ts
@@ -4,23 +4,18 @@ const cors = require("cors")
 
 dotenv.config();
 
-
-
-
 const app = express();
 const PORT = process.env.PORT || 5001;
-
 
 
 
 app.use(express.json());
 app.use(cors());
 
+import emailRoutes from "./API/emailRoutes"
+app.use("/api/v1/email", emailRoutes);
 
 
 const server = app.listen(PORT, () => {
     console.log(`server is listening in port:${PORT} :)`)
 })
-
-import emailRoutes from "./API/emailRoutes"
-app.use(`/api/v1/email`, emailRoutes);
